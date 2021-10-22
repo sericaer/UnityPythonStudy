18064329a851f42844471b031dcce69349fccb8e
sericaer <sericaer@outlook.com>
2021-10-22 15:21:21 +0800

    call python function from C#

---
+++ Assets/Plugin/Python/Examples/PythonCallFunc.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using IronPython.Hosting;
+using System;
+using UnityEngine.UI;
+
+namespace Exodrifter.UnityPython.Examples
+{
+    public class PythonCallFunc : MonoBehaviour
+    {
+        public Text text;
+
+        void Start()
+        {
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+
+            string code = @"
+def GetText(str):
+    return str + '_append'";
+
+            var source = engine.CreateScriptSourceFromString(code);
+            source.Execute(scope);
+
+            var funcGetText = scope.GetVariable<Func<string, string>>("GetText");
+            text.text = funcGetText("abc");
+        }
+    }
+}
